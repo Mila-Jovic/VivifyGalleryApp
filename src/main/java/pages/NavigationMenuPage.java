b4a75d9e0cba019c6b43cfa3f7dd301deff0093d
Mila Jovic <jovicmila205@gmail.com>
2022-06-30 09:42:16 +0200

    Adding LogoutTests with all necessary additions to pages

--- src/main/java/pages/NavigationMenuPage.java
+++ src/main/java/pages/NavigationMenuPage.java
@@ -10,41 +10,27 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 @Getter
-
 public class NavigationMenuPage extends BasePage {
     private By galleryAppLogo = By.xpath("//*[@id=\"navbarTogglerDemo01\"]/a");
     private By allGalleriesOption = By.xpath("//*[@id=\"navbarTogglerDemo01\"]/a");
     private By loginOption = By.xpath("//*[@id=\"navbarTogglerDemo01\"]/ul[2]/li[1]/a");
     private By registerOption = By.linkText("Register");
-    private By myGalleryOption = By.xpath("//*[@id=\"navbarTogglerDemo01\"]/ul[1]/li[2]/a");
-    private By createGallery = By.xpath("//*[@id=\"navbarTogglerDemo01\"]/ul[1]/li[3]/a");
     private By logoutOption = By.xpath("//*[@id=\"navbarTogglerDemo01\"]/ul[2]/li[3]/a");
 
 
     public NavigationMenuPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
-    public WebElement getMyGallery() {
-        return getDriver().findElement(myGalleryOption);
-    }
 
-    public void clickMyGalleryOptions() {
-        getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(myGalleryOption));
-        getMyGallery().click();
-    }
 
-    public WebElement getCreateGallery() {
-        return getDriver().findElement(createGallery);
-    }
-
-    public void clickCreateGallery() {
-        getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(createGallery));
-        getCreateGallery().click();
-    }
 
     public boolean isLogoutPresent() {
         getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(logoutOption));
         return getLogoutOptions().isDisplayed();
+    }
+    public boolean isLoginPresent() {
+        getDriverWait().until(ExpectedConditions.visibilityOfElementLocated(loginOption));
+        return getLoginOptions().isDisplayed();
     }
 
 
@@ -112,6 +98,7 @@ public class NavigationMenuPage extends BasePage {
         getDriver().navigate().to("https://gallery-app.vivifyideas.com/");
         clickRegisterOptions();
     }
+
     public void driverGoToLogin() {
         getDriver().navigate().to("https://gallery-app.vivifyideas.com/");
         clickLoginOptions();
---
+++ src/main/java/tests/LogoutTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LogoutTest extends BaseTestsPage{
+    @BeforeClass
+    public void setUp (){
+        getNavigationMenuPage().driverGoToLogin();
+        getLoginPage().login(getEmail(),getPassword());
+
+    }
+    @Test
+    public void logoutTest (){
+        getNavigationMenuPage().clickLogoutOptions();
+        Assert.assertTrue(getNavigationMenuPage().isLoginPresent());
+    }
+}
